f8add6d4967adbacb119692025f06e8fe0108005
nhkim55 <34884402+nhkim55@users.noreply.github.com>
2021-01-02 00:54:53 +0900

    Add files via upload

---
+++ code/some_module.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 17 11:21:17 2018
+
+@author: Administrator
+"""
+
+# some_module.py
+PI = 3.14159
+
+def f(x):
+    return x+2
+
+
+def g(a,b):
+    return a+b
